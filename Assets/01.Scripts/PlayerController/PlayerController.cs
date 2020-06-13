080e555e30d88d1e365941114e43dc546c937def
Komorio <twilivez@gmail.com>
2020-06-14 08:03:47 +0900

    [ADD] PlayerCharacterController 추가
    00. Mouse 위치와 가장 가까운 Vertical Line을 불러와 Left Value 및 Right Value를 받아옴
    01. 마우스 및 키보드를 통해 노드와 상호 작용

---
+++ Assets/01.Scripts/PlayerController/PlayerController.cs
@@ -0,0 +1,54 @@
+﻿using System.ComponentModel;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private int leftValue;
+    private int rightValue;
+
+    [Header("Objects")]
+    [SerializeField]
+    private VerticalLine[] verticalLines;
+
+    [Header("Events")]
+    [SerializeField]
+    private IntEvent leftEvent;
+
+    [SerializeField]
+    private IntEvent rightEvent;
+
+    private void Update(){
+        SetPosition();
+        GetAdjacentLineValue();
+
+        switch(Input.anyKeyDown){
+            case var k when Input.GetKeyDown(KeyCode.Z)||Input.GetMouseButtonDown(0):
+            leftEvent.Invoke(leftValue);
+            break; 
+
+            case var k when Input.GetKeyDown(KeyCode.X)||Input.GetMouseButtonDown(1):
+            rightEvent.Invoke(rightValue);   
+            break;
+        }
+    }
+
+    private void SetPosition(){
+        Vector2 newPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        gameObject.transform.position = newPosition;
+    }
+
+    private void GetAdjacentLineValue(){
+        var query = from o in verticalLines
+                orderby (gameObject.transform.position - o.gameObject.transform.position).sqrMagnitude
+                select o;
+
+        VerticalLine selectObject = query.FirstOrDefault();
+
+        leftValue = selectObject.LeftIndex;
+        rightValue = selectObject.RightIndex;
+    }
+
+}
