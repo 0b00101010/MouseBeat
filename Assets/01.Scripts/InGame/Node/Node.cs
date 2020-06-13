ea9f9c568967d7ebdbbc2dd67644e839053680de
Komorio <twilivez@gmail.com>
2020-06-14 07:12:05 +0900

    [ADD] NormalNode 추가

---
+++ Assets/01.Scripts/InGame/Node/Node.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField]
+    private int _score;
+
+    [SerializeField]
+    private int _reducedHp;
+
+    [SerializeField]
+    private float _defaultSpeed;
+
+    [SerializeField]
+    private float _judgePerfect;
+    
+    [SerializeField]
+    private float _judgeGreat;
+    
+    [SerializeField]
+    private float _judgeGood;
+
+    private SpriteRenderer _spriteRenderer;
+
+    protected int score => _score;
+    protected int reducedHp => _reducedHp;
+    protected float defaultSpeed => _defaultSpeed;    
+    
+    protected float judgePerfect => _judgePerfect;
+    protected float judgeGreat => _judgeGreat;
+    protected float judgeGood => _judgeGood;
+    
+    protected SpriteRenderer spriteRenderer => _spriteRenderer;
+    
+    private void Awake(){
+        _spriteRenderer = gameObject.GetComponent<SpriteRenderer>();
+    }
+
+    public virtual void Execute(Vector2 startPosition, Vector2 endPosition){ }
+
+    public virtual void Interaction(){ }
+    
+    public virtual void FailedInteraction(){}
+
+    public virtual void ObjectReset(){ 
+        gameObject.SetActive(false);
+    }   
+}
---
+++ Assets/01.Scripts/InGame/Node/NormalNode.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+using DG.Tweening;
+
+public class NormalNode : Node
+{
+    private Tween moveTween;
+
+    public override void Execute(Vector2 startPosition, Vector2 endPosition){
+        gameObject.SetActive(true);
+        gameObject.transform.position = startPosition;
+
+        moveTween = gameObject.transform.DOMove(endPosition, defaultSpeed);
+    }
+
+    public override void Interaction(){ 
+        int judgeLevel = 0;
+        switch(judgeLevel){
+            case var k when (judgePerfect - moveTween.position) < 0.03f:
+            judgeLevel = 4;
+            break;
+
+            case var k when moveTween.position > judgeGreat:
+            judgeLevel = 3;
+            break;
+            
+            case var k when moveTween.position > judgeGood:
+            judgeLevel = 2;
+            break;
+            
+            case var k when moveTween.position < judgeGood:
+            judgeLevel = 1;
+            break;
+        }
+
+        ObjectReset();
+        InGameManager.instance.scoreManager.GetScore(judgeLevel, score);
+    }   
+
+    public override void FailedInteraction(){
+        InGameManager.instance.scoreManager.GetScore(0, 0);
+        ObjectReset();
+    }   
+
+    public override void ObjectReset(){
+        moveTween?.Kill();
+        base.ObjectReset();
+    }
+
+    private void OnCollisionEnter2D(Collision2D other) {
+        if(other.collider.CompareTag("JudgeLine")){
+            FailedInteraction();
+        }    
+    }
+
+}
