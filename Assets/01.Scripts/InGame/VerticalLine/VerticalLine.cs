6f194dfb49fb14c3519d1911778c1a656dc521de
Komorio <twilivez@gmail.com>
2020-06-14 08:01:45 +0900

    [ADD] VericalLine 추가
    00. Line의 left area값 right area값을 갖고 있음

---
+++ Assets/01.Scripts/InGame/VerticalLine/VerticalLine.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VerticalLine : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField]
+    private int leftIndex;
+
+    [SerializeField]
+    private int rightIndex;
+
+    public int LeftIndex => leftIndex;
+    public int RightIndex => rightIndex;
+}
