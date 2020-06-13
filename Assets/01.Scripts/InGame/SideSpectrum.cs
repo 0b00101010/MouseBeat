9d6e2bd0cb75b778a85870fe38eaed8572cf626d
Komorio <twilivez@gmail.com>
2020-06-14 05:47:09 +0900

    [ADD] Bottom Spectrum 추가

---
+++ Assets/01.Scripts/InGame/SideSpectrum.cs
@@ -0,0 +1,24 @@
+﻿using System.Net.Mime;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SideSpectrum : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private Image[] sideSpectrums;
+
+    private float[] audioData = new float[2048];
+
+    private void Update(){
+        AudioListener.GetSpectrumData(audioData, 0, FFTWindow.Rectangular);
+
+        for(int i = 0; i < sideSpectrums.Length; i++){
+            Vector2 firstSclae = sideSpectrums[i].gameObject.transform.localScale;
+            firstSclae.y = audioData[i] * 60 + 1;
+            sideSpectrums[i].gameObject.transform.localScale = Vector2.MoveTowards(sideSpectrums[i].gameObject.transform.localScale, firstSclae, 0.1f);
+        }
+    }
+}
