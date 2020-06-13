8275a6a69ad4c4c61df2d7af91299c00e7caa76d
Komorio <twilivez@gmail.com>
2020-06-14 08:20:13 +0900

    [UPDATE] 판정 수정

--- Assets/01.Scripts/InGame/Node/NormalNode.cs
+++ Assets/01.Scripts/InGame/Node/NormalNode.cs
@@ -21,7 +21,7 @@ public class NormalNode : Node
     public override void Interaction(){ 
         int judgeLevel = 0;
         switch(judgeLevel){
-            case var k when judgePerfect == moveTween.position:
+            case var k when (judgePerfect - moveTween.position) < 0.005f:
             judgeLevel = 4;
             break;
 
