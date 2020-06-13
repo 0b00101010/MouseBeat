7a4515dc6840614e8393f9b3dff4191059234235
Komorio <twilivez@gmail.com>
2020-06-14 05:21:51 +0900

    [ADD] BackgroundController 추가
    00. LineRenderer 를 통해 배경 색 변경

---
+++ Assets/01.Scripts/InGame/BackgroundController.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundController : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private LineRenderer backgroundRenderer;
+
+    private Color topColor;
+    private Color bottomColor;
+
+    private IEnumerator executeCoroutine;
+
+    private void Awake(){
+        topColor = backgroundRenderer.startColor;
+        bottomColor = backgroundRenderer.endColor;
+    }
+
+    public void Execute(){
+        executeCoroutine?.Stop(this);
+        executeCoroutine = ExecuteCoroutine().Start(this);
+    }
+
+    private IEnumerator ExecuteCoroutine(){
+        Color beforeTopColor = topColor;
+        Color beforeBottomColor = bottomColor;
+
+        topColor = RandomColor();
+        bottomColor = RandomColor();
+
+        for(int i = 0; i < 60; i++){
+            backgroundRenderer.startColor = Color.Lerp(beforeTopColor, topColor, i / 60.0f);
+            backgroundRenderer.endColor = Color.Lerp(beforeBottomColor, bottomColor, i / 60.0f);
+
+            yield return YieldInstructionCache.WaitSeconds(0.05f);
+        }
+    }
+
+    private Color RandomColor(){
+        return Color.HSVToRGB(Random.value, 1, 1);
+    }
+
+}
---
+++ Assets/01.Scripts/InGame/InGameManager.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using NaughtyAttributes;
+
+public class InGameManager : MonoBehaviour
+{
+
+    private BackgroundController backgroundController;
+
+    private void Awake(){
+        backgroundController = gameObject.GetComponent<BackgroundController>();
+    }
+
+    [Button("Change Background Color")]
+    private void ChangeBackgroundColor() { 
+        backgroundController.Execute();
+    }
+
+}
