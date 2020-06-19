985041c8034977780711472e4591fab2dd136c43
Komorio <twilivez@gmail.com>
2020-06-19 10:26:14 +0900

    [ADD] 파일 읽고 Node 만드는 기능 추가

---
+++ Assets/01.Scripts/InGame/Manager/SongHandler.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Linq;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SongHandler : MonoBehaviour
+{
+    private AudioSource audioSource;
+    private float songProcess;
+
+    private string[] mapFileStrings;
+
+    private Dictionary<string, float> gameSettings = new Dictionary<string, float>(); 
+    private List<SongProcessAction> songAction = new List<SongProcessAction>();
+
+    private void Start(){
+        audioSource = gameObject.GetComponent<AudioSource>();
+        ReadFile(); 
+    }
+
+    private void Update(){
+        songProcess = (audioSource.time / audioSource.clip.length);
+        
+        NodeGenerate();
+        NodeGenerate();
+    
+    }
+
+    private void NodeGenerate(){
+        if(Mathf.Abs((songAction[0].position - songProcess)) < 0.0005){
+            songAction[0].action();
+            songAction.Remove(songAction[0]);
+        }
+    }
+
+    private void ReadFile(){
+        var tempString = Resources.Load<TextAsset>("MapFile/StepEdit").text;
+        mapFileStrings = tempString.Split('\n');
+        
+        var count = 0;
+
+        for(int i = 0; i < mapFileStrings.Length; i++){
+            if(!mapFileStrings[i].StartsWith(":")){
+                count = i;
+            }
+        }
+
+        for(int i = 0; i < mapFileStrings.Length; i++){
+            if(!mapFileStrings[i].StartsWith(":")){
+                float processValue = (float)i / (float)count;
+                
+                List<int> positionValues = mapFileStrings[i].IndexOfMany("X").ToList();
+
+                if(positionValues.Count > 0){
+                    for(int j = 0; j < positionValues.Count; j++){
+                        SongProcessAction processAction = new SongProcessAction();
+
+                        processAction.position = processValue;
+                        processAction.action = NormalNodeGenerateAction(positionValues[j]);
+                        
+                        songAction.Add(processAction);
+                    }
+                }
+
+                positionValues.Clear();
+
+                positionValues = mapFileStrings[i].IndexOfMany("M").ToList();
+
+                if(positionValues.Count > 0){
+                    for(int j = 0; j < positionValues.Count; j++){
+                        SongProcessAction processAction = new SongProcessAction();
+
+                        processAction.position = processValue;
+                        processAction.action = LongNodeStartGenerateAction(positionValues[j]);
+                        
+                        songAction.Add(processAction);
+                    }
+                }
+
+                positionValues.Clear();
+
+                positionValues = mapFileStrings[i].IndexOfMany("W").ToList();
+
+                if(positionValues.Count > 0){
+                    for(int j = 0; j < positionValues.Count; j++){
+                        SongProcessAction processAction = new SongProcessAction();
+
+                        processAction.position = processValue;
+                        processAction.action = LongNodeEndGenerateAction(positionValues[j]);
+                        
+                        songAction.Add(processAction);
+                    }
+                }
+
+            } else {
+                var tempStrings = mapFileStrings[i].Split(':')[1].Split('=');
+                GameSettingAction(tempStrings[0], float.Parse(tempStrings[1]));
+            }
+        }
+    }
+
+    private Action NormalNodeGenerateAction(int positionValue){
+        return () => {
+            InGameManager.instance.nodeGenerator.NormalNodeGenerate(positionValue);
+        };
+    }
+
+    private Action LongNodeStartGenerateAction(int positionValue){
+        return () => {
+            InGameManager.instance.nodeGenerator.LongNodeGenerate(positionValue);
+        };
+    }
+
+    private Action LongNodeEndGenerateAction(int positionValue){
+        return () => {
+            InGameManager.instance.nodeGenerator.LongNodeStop(positionValue);
+        };
+    }
+
+    private Action GameSettingAction(string valueName, float value){
+        return () => {
+            Setting(valueName, value);
+        };
+    }
+
+    private void Setting(string valueName, float value){
+        if(gameSettings.ContainsKey(valueName)){
+            gameSettings[valueName] = value;
+        }else{
+            gameSettings.Add(valueName, value);
+        }
+    }
+}
---
+++ Assets/01.Scripts/InGame/Manager/SongProcessAction.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+
+[Serializable]
+public class SongProcessAction{
+    [SerializeField]
+    private float _position;
+    
+    [SerializeField]
+    private Action _action;
+
+    public float position{
+        get{
+            return _position;
+        }
+
+        set{
+            _position = value;
+        }
+    }
+
+    public Action action{
+        get{
+            return _action;
+        }
+
+        set{
+            _action = value;
+        }
+    }
+}
