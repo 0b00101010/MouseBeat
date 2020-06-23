039ecd8e6302f2893c59afb1683526bd197dba17
Komorio <twilivez@gmail.com>
2020-06-23 09:18:33 +0900

    [ADD] StartScene Background Circle 추가

---
+++ Assets/01.Scripts/StartScene/BackgroundCircle.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BackgroundCircle : MonoBehaviour
+{
+    [Header("Values")]
+    [SerializeField]
+    private float rotateAngle;
+
+    private void Awake(){
+        RotateCoroutine().Start(this);
+    }
+
+    private IEnumerator RotateCoroutine(){
+        while(true){
+            gameObject.transform.Rotate(Vector3.forward * rotateAngle);
+            yield return YieldInstructionCache.WaitFrame;
+        }
+    }
+}
