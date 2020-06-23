c4e84ec4a844e5a4b4070c5a6155166fe1564a36
Komorio <twilivez@gmail.com>
2020-06-23 09:18:56 +0900

    [ADD] Panel 을 컨트롤 할 UIWidget 스크립트 추가

---
+++ Assets/01.Scripts/StartScene/StartSceneManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class StartSceneManager : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private Image[] logoImages;
+
+    [SerializeField]
+    private UIWidget titleWidget;
+
+    [SerializeField]
+    private UIWidget stageSelectWidget; 
+
+    private bool isTitle = true;
+
+    public void Update(){
+        if(Input.GetMouseButtonDown(0)){
+            if(isTitle){
+                titleWidget.CloseWidget(stageSelectWidget.OpenWidget);
+            } else {
+                
+            }
+        }
+    }
+}
---
+++ Assets/01.Scripts/StartScene/UIWidget.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+public class UIWidget : MonoBehaviour
+{
+    [Header("Objects")]
+    [SerializeField]
+    private Image[] items;
+
+    [Header("Values")]
+    [SerializeField]
+    private float duration;
+
+    private Tween widgetTween;
+
+    public void OpenWidget(){
+        gameObject.SetActive(true);
+
+        for(int i = 0; i < items.Length; i++){
+            if(i.Equals(items.Length - 1)){
+                widgetTween = items[i].DOFade(1, duration);
+            } else {
+                items[i].DOFade(1, duration);
+            }
+        }
+    }
+
+    public void OpenWidget(Action callback = null){
+        gameObject.SetActive(true);
+
+        for(int i = 0; i < items.Length; i++){
+            if(i.Equals(items.Length - 1)){
+                widgetTween = items[i].DOFade(1, duration);
+            } else {
+                items[i].DOFade(1, duration);
+            }
+        }
+
+        widgetTween.OnComplete(() => {
+            callback();
+        });
+    }
+
+    public void CloseWidget(){
+        for(int i = 0; i < items.Length; i++){
+            if(i.Equals(items.Length - 1)){
+                widgetTween = items[i].DOFade(0, duration);
+            } else {
+                items[i].DOFade(0, duration);
+            }
+        }
+
+        widgetTween.OnComplete(() => {
+            gameObject.SetActive(false);
+        });
+    }    
+    
+    public void CloseWidget(Action callback = null){
+        for(int i = 0; i < items.Length; i++){
+            if(i.Equals(items.Length - 1)){
+                widgetTween = items[i].DOFade(0, duration);
+            } else {
+                items[i].DOFade(0, duration);
+            }
+        }
+
+        widgetTween.OnComplete(() => {
+            gameObject.SetActive(false);
+            callback();
+        });
+    }
+}
