2ae6941d530daaa815fa5bf2db8f908b7b16a16f
Komorio <twilivez@gmail.com>
2020-06-28 16:45:35 +0900

    [ADD] Title Scene Touch To Start 추가

---
+++ Assets/01.Scripts/StartScene/TouchToStart.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using DG.Tweening;
+
+public class TouchToStart : MonoBehaviour
+{
+    private Image touchToStart;
+    private Tween fadeTween;
+    private IEnumerator fadeCoroutine;
+
+    private void Awake(){
+        touchToStart = gameObject.GetComponent<Image>();
+    }
+
+    private void Start(){
+        fadeCoroutine = FadeCoroutine().Start(this);
+    }
+
+    private IEnumerator FadeCoroutine(){
+        while(true){
+            fadeTween = touchToStart.DOFade(0, 1.25f);
+            yield return fadeTween.WaitForCompletion();
+            
+            fadeTween = touchToStart.DOFade(1, 1.25f);
+            yield return fadeTween.WaitForCompletion();
+        }
+    }
+
+    private void OnDisable() {
+        fadeCoroutine.Stop(this);
+    }
+}
